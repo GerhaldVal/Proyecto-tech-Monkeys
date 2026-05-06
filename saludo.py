8712807b48e5e8e80c26c4aca7c35e9d2f57e70a
GerhaldVal <gerhald.valenzuela.flores@alumnos.uta.cl>
2026-05-06 11:08:08 -0400

    Create saludo.py

---
+++ saludo.py
@@ -0,0 +1,15 @@
+
+nombre=input("hola ,como te llamas?")
+
+edad=input("Cual es tu edad?")
+print("""MENU:
+      1) SUMAR 
+      2) RESTAR
+      3) MULTIPLICAR
+      4) DIVIDIR
+      5) SALIR""")
+lol=int(input("Que opcion quieres?"))
+#while lol !=5 :
+    #match lol:
+        #case 1:
+            
