e17055ce6d3837c2a79ab045d739771ae30b4e0e
nicoher135 <nicolas.guajardo.hernandez@alumnos.uta.cl>
2026-05-08 11:31:16 -0400

    Calculadora.py

--- saludo.py
+++ saludo.py
@@ -1,7 +1,6 @@
 
-nombre=input("hola ,como te llamas?")
-
-edad=input("Cual es tu edad?")
+numero1=int(input("Ingrese un numero"))
+numero2=int(input("Ingrese  otro numero"))
 print("""MENU:
       1) SUMAR 
       2) RESTAR
@@ -9,7 +8,24 @@ print("""MENU:
       4) DIVIDIR
       5) SALIR""")
 lol=int(input("Que opcion quieres?"))
-#while lol !=5 :
-    #match lol:
-        #case 1:
+while lol !=5 :
+    match lol:
+      case 1:
+            total=numer1+numero2
+            print("El resultado final es",total)
+      case 2:
+            total=numero1-numero2
+            print("El resultado final es",total)
+      case 3:
+            total=numero1*numero2
+            print("El resultado final es",total)
+      case 4:
+            if numero2!=0:
+                  total=numero1/numero2
+                  print("El resultado final es",total)
+            else:
+                  print("no se puede dividir por cero")
+   lol=int(input("Que opcion quieres?"))
+              
+            
             
