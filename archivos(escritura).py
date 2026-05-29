9202ed5a698988bfdaedc5bc50d49e25028eea5d
GerhaldVal <gerhald.valenzuela.flores@alumnos.uta.cl>
2026-05-29 12:49:21 -0400

    Create archivos(escritura).py

---
+++ archivos(escritura).py
@@ -0,0 +1,5 @@
+with open("prueba_de_archivo_2.txt", "w", encoding="utf-8" ) as archivo: #aqui utilizamos el comando "w" (write) para crear y escribir un archivo con las lineas puestas en el comando
+    archivo.write ("estamos creando este archivo a traves de la terminal python \n")
+    archivo.write ("esta es una linea seguida a la anterior \n")
+
+   
