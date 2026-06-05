5e3f5f4052086132e486b8434fb3496a951ada3a
GerhaldVal <gerhald.valenzuela.flores@alumnos.uta.cl>
2026-06-05 12:19:03 -0400

    Update archivos(escritura).py

--- archivos(escritura).py
+++ archivos(escritura).py
@@ -1,5 +1,8 @@
-with open("prueba_de_archivo_2.txt", "w", encoding="utf-8" ) as archivo: #aqui utilizamos el comando "w" (write) para crear y escribir un archivo con las lineas puestas en el comando
-    archivo.write ("estamos creando este archivo a traves de la terminal python \n")
-    archivo.write ("esta es una linea seguida a la anterior \n")
-
+with open("Menu_Creado_con_archivo,write).txt", "w", encoding="utf-8" ) as archivo: #aqui utilizamos el comando "w" (write) para crear y escribir un archivo con las lineas puestas en el comando
+    archivo.write ("################################## \n")
+    archivo.write ("########### Menu ################# \n")
+    archivo.write ("########### Opcion 1 ############# \n")
+    archivo.write ("########### Opcion 2 ############# \n")
+    archivo.write ("########### Opcion 3 ############# \n")
+    archivo.write ("########### Opcion 4 ############# \n")  # lo dejamos abierto en caso de querer añadir mas opcines "con with open , a"
    
