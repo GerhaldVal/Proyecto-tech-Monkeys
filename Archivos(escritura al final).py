280eefd4f9076edf1546153d6bda3c7085e8fd8f
GerhaldVal <gerhald.valenzuela.flores@alumnos.uta.cl>
2026-06-05 11:43:47 -0400

    Update Archivos(escritura al final).py

--- Archivos(escritura al final).py
+++ Archivos(escritura al final).py
@@ -1,2 +1,2 @@
 with open ("prueba_de_archivo_2.txt","a", encoding = "utf-8" ) as archivo: # aqui utilizamos el comando "a" (at end) para escribir archivos al final del texto sin sobreescribir el antiguo archivo
-        archivo.write ("se nos olvido añadir esta linea asi que la añadimos abajo \n")
+        archivo.write ("esta linea se añade al final del archivo gracias al comando with open , a \n")
