61661ab4507ba76ebf60db9f9b7dcd4034639e85
GerhaldVal <gerhald.valenzuela.flores@alumnos.uta.cl>
2026-06-05 12:19:05 -0400

    Update Archivos(lectura).py

--- Archivos(lectura).py
+++ Archivos(lectura).py
@@ -1,3 +1,19 @@
-with open ('ejemplo.txt','r', encoding = 'utf-8') as archivo: # Esto sirve para abrir y cerrar el archivo directamente despues de usarlo, encoding "utf-8" ayuda a que python no tenga problemas al leer el archivo
+with open ('Menu_Creado_con_archivo,write','r', encoding = 'utf-8') as archivo: # Esto sirve para abrir y cerrar el archivo directamente despues de usarlo, encoding "utf-8" ayuda a que python no tenga problemas al leer el archivo
     contenido = archivo.read()
     print(contenido)
+opcion=int(input("eliga una opcion \n "))
+match opcion:
+    case 1:
+        print("gracias por elegir")
+    case 2:
+        print("gracias por elegir")
+    case 3:
+        print("gracias por elegir")
+    case 4:
+        print("gracias por elegir")
+    case 5:
+        print("gracias por elegir")
+print("salio del menu")
+
+    
+    
