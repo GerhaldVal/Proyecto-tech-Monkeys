8047a9d4ee239069cefced1ec9a09df4f215fc69
vigoco0 <vicente.gomez.cortes@alumnos.uta.cl>
2026-06-24 10:56:43 -0400

    Add files via upload

---
+++ ejercicio_class1.py
@@ -0,0 +1,9 @@
+class Elevador:
+    def __init__(self,marca,modelo,piso):
+        self.marca = marca
+        self.modelo = modelo
+        self.piso = piso
+    def piso(self):
+        input(int(print("Ingrese el piso al que quiere llegar\n")))
+
+ 
