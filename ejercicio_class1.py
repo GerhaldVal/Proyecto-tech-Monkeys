4823e4f2d73338bd4f48c4569e1ec2a7a090a8bb
vigoco0 <vicente.gomez.cortes@alumnos.uta.cl>
2026-06-25 00:27:33 -0400

    Update ejercicio_class1.py

--- ejercicio_class1.py
+++ ejercicio_class1.py
@@ -1,9 +1,28 @@
 class Elevador:
-    def __init__(self,marca,modelo,piso):
+    def __init__(self, marca, modelo, piso,piso_bloqueado):
         self.marca = marca
         self.modelo = modelo
         self.piso = piso
-    def piso(self):
-        input(int(print("Ingrese el piso al que quiere llegar\n")))
+        self.piso_bloqueado = piso_bloqueado
+
+    def seleccionar_piso(self):
+        self.piso = int(input("Ingrese el piso al que quiere llegar: "))
+
+        if 1 <= self.piso <= 3:
+            print(f"Usted ha llegado al piso: {self.piso}")
+        else:
+            print("Piso no válido")
+    def bloqueo_de_piso(self):
+        self.piso_bloqueado = int(input("Ingrese el piso al que quiere llegar: "))
+        if self.piso_bloqueado == self.piso:
+            print("Piso bloqueado (Usted esta aqui).")
+        elif 1<= self.piso_bloqueado <= 3:
+            print("Ha llegado al piso.")
+        else:
+            print("Piso inexistente.")
+
+AlturaSegura = Elevador("Samsung", "V4", 1,8)
+AlturaSegura.seleccionar_piso()
+AlturaSegura.bloqueo_de_piso()
 
  
