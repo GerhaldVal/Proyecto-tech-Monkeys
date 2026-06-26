d1e67343fa2eeb72eae4d03b86361a30a00ef48e
vigoco0 <vicente.gomez.cortes@alumnos.uta.cl>
2026-06-26 11:59:54 -0400

    Update ejercicio_class1.py

--- ejercicio_class1.py
+++ ejercicio_class1.py
@@ -6,21 +6,20 @@ class Elevador:
         self.piso_bloqueado = piso_bloqueado
 
     def seleccionar_piso(self):
-        self.piso = int(input("Ingrese el piso al que quiere llegar: "))
+        self.piso = int(input("Ingrese el piso al que quiere llegar (1,2 o 3): "))
 
         if 1 <= self.piso <= 3:
             print(f"Usted ha llegado al piso: {self.piso}")
         else:
             print("Piso no válido")
     def bloqueo_de_piso(self):
-        self.piso_bloqueado = int(input("Ingrese el piso al que quiere llegar: "))
+        self.piso_bloqueado = int(input("Ingrese el piso al que quiere llegar (1,2 o 3): "))
         if self.piso_bloqueado == self.piso:
-            print("Piso bloqueado (Usted esta aqui).")
+            print(f"Piso bloqueado (Usted esta aqui (Piso {self.piso_bloqueado})).")
         elif 1<= self.piso_bloqueado <= 3:
-            print("Ha llegado al piso.")
+            print(f"Ha llegado al piso: {self.piso_bloqueado}")
         else:
             print("Piso inexistente.")
-
 AlturaSegura = Elevador("Samsung", "V4", 1,8)
 AlturaSegura.seleccionar_piso()
 AlturaSegura.bloqueo_de_piso()
